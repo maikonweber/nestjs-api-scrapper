67b23426c2d6ac44c8ef82a90d369f3f76d1b9d5
MaikonWeber <maikonweber@gmail.com>
2024-01-18 17:34:39 -0300

    Add Preload Chrome

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  preload(): Promise<string> {
+    return this.appService.preload();
+  }
+
   @Get(':number:message')
-  getHello(@Param('number') number: number,@Param('message') message: string): Promise<string> {
-    return this.appService.getHello(number, message);
+  sendMensagem(@Param('number') number: number,@Param('message') message: string): Promise<string> {
+    return this.appService.getNumberMensage(number, message)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,19 +3,27 @@ import * as puppeteer from 'puppeteer';
 
 @Injectable()
 export class AppService {
-  async getHello(phone :number, message: string): Promise<string> {
+  public page;
+  async preload(): Promise<string> {
     const browser = await puppeteer.launch({
       // Add any necessary options here
       headless: false,
       userDataDir: 'data/userdata',
-
     });
-    const page = await browser.newPage();
-    await page.goto('https://web.whatsapp.com/send?phone=55')
-    await new Promise(r => setTimeout(r, 120000));
-    let http = `https://web.whatsapp.com/send?phone=55' + ${phone} &text=${message}`
+
+    this.page = await browser.newPage();
+    await this.page.goto('https://web.whatsapp.com/')
     await new Promise(r => setTimeout(r, 65000));
-    await page.keyboard.press('Enter')
+ 
     return "Hello World";
+  }
+
+  async getNumberMensage(phone: number, message: string) {
+    let http = `https://web.whatsapp.com/send?phone=55' + ${phone} &text=${message}`
+    this.page.goto(http);
+    await new Promise(r => setTimeout(r, 65000));
+    this.page.keyboard.press('Enter');
+
+    return "Mensage Send"
   }
 }
