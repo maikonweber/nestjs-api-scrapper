a770ce9dd31e465583595cf43c65252715fb9516
MaikonWeber <maikonweber@gmail.com>
2024-02-01 13:12:10 -0300

    Add TE

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,17 @@ import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksService } from './tasks-service/tasks-service.service';
 import { VideoMakerService } from './video-maker/video-maker.service';
+import { TelegramService } from './telegram/telegram.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration module global
+    })
   ],
   controllers: [AppController],
-  providers: [AppService, TasksService, VideoMakerService],
+  providers: [AppService, TasksService, VideoMakerService, TelegramService],
 })
 export class AppModule {}
---
+++ src/telegram/telegram.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import * as FormData from 'form-data';
+
+import axios from 'axios';
+
+@Injectable()
+export class TelegramService {
+  private readonly logger = new Logger(TelegramService.name);
+  private readonly botToken: string;
+  private readonly chatId: string;
+  private readonly apiUrl: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.botToken = configService.get<string>('TELEGRAM_TOKEN');
+    this.chatId = this.configService.get<string>('TELEGRAM_CHAT_ID');
+    this.apiUrl = `https://api.telegram.org/bot${this.botToken}`;
+  }
+
+  async sendMessageToSpecificChat(
+    message: string,
+    specificChatId: string,
+  ): Promise<void> {
+    try {
+      const response = await axios.post(`${this.apiUrl}/sendMessage`, {
+        chat_id: specificChatId,
+        text: message,
+      });
+
+      if (response.data.ok !== true) {
+        throw new Error(`Failed to send message: ${response.data.description}`);
+      }
+    } catch (error) {
+      // Handle errors appropriately
+      console.error(error.message);
+      throw error;
+    }
+  }
+  
+  async uploadVideo(chatId: string, videoPath: string): Promise<any> {
+    const url = `${this.apiUrl}${this.botToken}/sendVideo`;
+
+    const formData = new FormData();
+    formData.append('chat_id', chatId);
+    formData.append('video', fs.createReadStream(videoPath));
+
+    const headers = {
+      ...formData.getHeaders(),
+    };
+
+    const response = await axios.post(url, formData, { headers });
+
+    return response.data;
+  }
+
+}
