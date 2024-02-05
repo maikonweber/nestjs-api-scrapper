6612268bb271e233e0ba083776217bfdcbb12a18
MaikonWeber <maikonweber@gmail.com>
2024-02-05 16:01:16 -0300

    Add Cron Job for duas horas sendvideo

--- src/tasks-service/tasks-service.service.ts
+++ src/tasks-service/tasks-service.service.ts
@@ -1,14 +1,25 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule'
 import { AppService } from 'src/app.service';
+import { TelegramService } from 'src/telegram/telegram.service';
+import { VideoMakerService } from 'src/video-maker/video-maker.service';
 
 @Injectable()
 export class TasksService {
     private readonly logger = new Logger(TasksService.name);
-    constructor(private readonly apiService: AppService) {
+    constructor(
+        private readonly apiService: AppService,
+        private readonly videoMaker: VideoMakerService,
+        private readonly telegramService: TelegramService
+        ) {
         
     }
-    // @Cron('45 * * * * *')
+    @Cron('0 */2 * * *')
+    
+    async handleVideoMensage(){
+      const nameString = await this.videoMaker.createNewVdo()
+      return this.telegramService.uploadVideo('-4057079951', nameString)
+    }
     // async handleCron() {
     //     const number: string = `119${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`
     //     this.logger.debug("Called when the current second is 45", number);
